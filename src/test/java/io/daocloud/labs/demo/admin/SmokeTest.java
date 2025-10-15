7d6cd9530b3a9cf1997bb7e23ff01c82c278877d
Lemon <2086355155@qq.com>
2025-10-15 09:01:15 +0000

    add src/test/java/io/daocloud/labs/demo/admin/SmokeTest.java.
    Signed-off-by: Lemon <2086355155@qq.com>

---
+++ src/test/java/io/daocloud/labs/demo/admin/SmokeTest.java
@@ -0,0 +1,10 @@
+package io.daocloud.labs.demo.admin;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class SmokeTest {
+    @Test public void ok() {
+        assertTrue(true);
+    }
+}
