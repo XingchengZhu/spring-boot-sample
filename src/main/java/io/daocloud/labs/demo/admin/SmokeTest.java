2b59bbe75cf0613d6109c224aaa03a42fa10384a
Lemon <2086355155@qq.com>
2025-10-15 09:04:45 +0000

    删除文件 src/main/java/io/daocloud/labs/demo/admin/SmokeTest.java

--- src/main/java/io/daocloud/labs/demo/admin/SmokeTest.java
+++
@@ -1,8 +0,0 @@
-package io.daocloud.labs.demo.admin;
-
-import org.junit.Test;
-import static org.junit.Assert.assertTrue;
-
-public class SmokeTest {
-    @Test public void ok() { assertTrue(true); }
-}
